f5221b70b3fa179a9463204e6513caea47df2912
noman291 <aanoman291@gmail.com>
2025-11-30 01:54:10 +0600

    setup firebase in context

--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import { auth } from "../fireBaseConfiq/fireBaseConfiq";
 
 const AuthProvider = ({ children }) => {
-  const value = {};
+  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
+  const [loadding, setloadding] = useState(true);
+  // signIn with Google
+  const googleSignIn = () => {
+    signInWithPopup(auth, provider);
+  };
+  // create user with email
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  // signin with email
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // UserManage
+
+  useEffect(() => {
+    const unsubsCribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setloadding(false);
+      console.log("current userInfo", currentUser || "empty");
+    });
+    return unsubsCribe;
+  }, []);
+  const value = { user, loadding, setUser, signInUser, createUser, signInUser };
   return <AuthContext value={value}>{children}</AuthContext>;
 };
 
