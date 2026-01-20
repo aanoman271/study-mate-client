c88b8fb51f04f0c009467c9534cc9685d8409110
noman291 <aanoman291@gmail.com>
2026-01-20 19:40:52 +0600

    headder update

--- src/components/SecoundHeader.jsx
+++ src/components/SecoundHeader.jsx
@@ -5,13 +5,13 @@ import useAuth from "../hook/useAuth";
 const SecoundHeader = () => {
   const { user } = useAuth();
   return (
-    <div className="md:flex hidden max-w-7xl h-15 relative mx-auto justify-between ">
-      <div className="">
+    <div className="flex  max-w-7xl h-15 relative mx-auto md:justify-between justify-center">
+      <div className="hidden md:flex">
         <p className="text-secondary bottom-0 absolute font-semibold">
           Connect with learners{" "}
         </p>
       </div>
-      <div className=" bg-gradient-to-br from-purple-600 to-indigo-600 rounded-tl-full w-[50%] flex justify-end gap-5 items-center pr-3">
+      <div className=" bg-gradient-to-br from-purple-600 to-indigo-600 rounded-tl-full md:w-[50%] w-full flex justify-end gap-5 items-center pr-3">
         {user ? (
           <small className="text-right text-gray-300 tracking-wide">
             <span className="text-[16px] font-semibold text-primary">
